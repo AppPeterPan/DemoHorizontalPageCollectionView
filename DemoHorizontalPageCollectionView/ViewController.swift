29fdfa4c7bc5b711a0b266d1ac753417ccfc8832
peterpan <apppeterpan@gmail.com>
2022-04-14 14:12:10 +0800

    Initial Commit

---
+++ DemoHorizontalPageCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoHorizontalPageCollectionView
+//
+//  Created by Peter Pan on 2022/4/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
