c47871136892d0b694e67574f9384b743189c243
peterpan <apppeterpan@gmail.com>
2022-04-14 15:25:11 +0800

    update

---
+++ DemoHorizontalPageCollectionView/NumberCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NumberCollectionViewCell.swift
+//  DemoHorizontalPageCollectionView
+//
+//  Created by Peter Pan on 2022/4/14.
+//
+
+import UIKit
+
+class NumberCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = "\(NumberCollectionViewCell.self)"
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
--- DemoHorizontalPageCollectionView/ViewController.swift
+++ DemoHorizontalPageCollectionView/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let photos = (1...10).map { "\($0).circle.fill" }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NumberCollectionViewCell.reuseIdentifier, for: indexPath) as! NumberCollectionViewCell
+        let photo = photos[indexPath.item]
+        cell.imageView.image = UIImage(systemName: photo)
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.bounds.size
+    }
+}
